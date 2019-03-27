a7647e41ab1dbbda38c6c94b3022bc54161f020f
Diamond <me@diamondsystems.org>
2019-03-27 08:43:30 +0500

    code styles

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,20 @@
-<?php namespace Wbry\Content;
+<?php
+
+namespace Wbry\Content;
 
 use System\Classes\PluginBase;
 
+/**
+ * Plugin - Content control
+ *
+ * @package Wbry\Content
+ * @author Diamond Systems
+ */
 class Plugin extends PluginBase
 {
     public function registerComponents()
-    {
-    }
+    {}
 
     public function registerSettings()
-    {
-    }
+    {}
 }
