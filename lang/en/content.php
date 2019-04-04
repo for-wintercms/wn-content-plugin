30ffe50fdcccf53595c18a7dd1b7f75a6917458e
Diamond <me@diamondsystems.org>
2019-04-04 14:13:48 +0500

    added title field

--- lang/en/content.php
+++ lang/en/content.php
@@ -1,8 +1,9 @@
 <?php return [
     'prompt' => 'Add new item',
     'items' => [
-        'name_label' => 'Name',
-        'name_cmt'   => 'The name by which you will receive the content. Valid characters: a-z_-',
+        'title_label' => 'Name',
+        'name_label'  => 'Key',
+        'name_cmt'    => 'The name by which you will receive the content. Valid characters: a-z_-',
         'repeater_label' => 'Repeater',
         'repeater_cmt'   => 'Repeater pattern.',
         'repeater_emptyOption' => '--- Repeater pattern ---',
