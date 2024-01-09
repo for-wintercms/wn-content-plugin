362a4526326cd267f7cbafba497aabd974bafb6b
Almaz <me@diamondsystems.org>
2024-01-09 11:49:49 +0500

    Switch translated fields

--- assets/js/backend/translate_items.js
+++ assets/js/backend/translate_items.js
@@ -1,12 +1,25 @@
 var translate_items = translate_items || {
 
+    switchingTimeTranslatedFields: 200, // in milliseconds
+
     addFileLangMark: function()
     {
         $("#layout-body .form-group [translate='1']").each(function(i, el)
         {
             $formGroup = $(el).closest('.form-group');
+            $formGroup.addClass('translate-field');
             if ($formGroup.children().prop('tagName').toLowerCase() === 'label')
                 $formGroup.find('label:first-child').append('<span class="file-lang-mark">'+$formGroup.data('field-name')+'</span>');
+        });
+    },
+
+    switchTranslatedFields: function(lang, switchTimeMs)
+    {
+        $("#layout-body .translate-field").each(function(i, el){
+            if ($(el).data('field-name') === lang)
+                $(el).show(switchTimeMs);
+            else
+                $(el).hide(switchTimeMs);
         });
     },
 
@@ -123,20 +136,18 @@ var translate_items = translate_items || {
     {
         var me = this;
 
-        // TODO: css class "file-lang-mark"
-
-        console.log('new translate item JS');
-
-        // ajax before send
-        $(window).on('ajaxBeforeSend', function(event, context) {
-            //---
-            console.log(context);
-            //---
-        });
-
+        // Switch translated fields
+        me.switchTranslatedFields($('#translateTabs li.active:first').data('lang').toUpperCase(), 0);
         $('#translateTabs li').click(function(){
-            console.log($(this).text());
+            me.switchTranslatedFields($(this).data('lang').toUpperCase(), me.switchingTimeTranslatedFields);
         });
+
+        // // ajax before send
+        // $(window).on('ajaxBeforeSend', function(event, context) {
+        //     //---
+        //     console.log(context);
+        //     //---
+        // });
 
         // Fill in blanks and Save
         /*
