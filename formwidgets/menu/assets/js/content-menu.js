a5a129f27293a2b0f7363acdd8ab8432daa77daa
Almaz <me@diamondsystems.org>
2024-01-24 15:39:37 +0500

    A form to fill in the menu buttons and an accordion for the menu

--- formwidgets/menu/assets/js/content-menu.js
+++ formwidgets/menu/assets/js/content-menu.js
@@ -1,3 +1,38 @@
+/**
+ * ContentMenu widget
+ */
+
+const contentMenu = {
+
+    menuItemToggleAnimateInMs: 100,
+
+    events: function ()
+    {
+        const me = this;
+
+        // Toggle menu
+        $('.field-contentmenu').on('click', '.content-menu > .menu-item > .menu-header', function()
+        {
+            if ($(this).hasClass('active'))
+            {
+                $(this).removeClass('active').next().hide(me.menuItemToggleAnimateInMs);
+                return;
+            }
+
+            const $contentMenu = $(this).closest('.content-menu');
+            $contentMenu.find('.menu-header').removeClass('active');
+            $contentMenu.find('.menu-body').hide();
+
+            $(this).addClass('active').next().show(me.menuItemToggleAnimateInMs);
+        });
+    },
+
+    init: function ()
+    {
+        this.events();
+    }
+};
+
 $(document).ready(function(){
-    //---
+    contentMenu.init();
 });
--- formwidgets/menu/partials/_menu.php
+++ formwidgets/menu/partials/_menu.php
@@ -21,18 +21,94 @@ $fieldName = $field->getName();
             <div class="menu-item">
                 <div class="menu-header"><h3>#1 menu-header</h3><i class="icon-angle-right"></i></div>
                 <div class="menu-body">
-                    FORM
+                    <div class="row menu-item-form">
+
+<!--                        https://www.w3schools.com/tags/tag_a.asp -->
+
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" placeholder="Name" aria-label="Name">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" placeholder="Css class" aria-label="Css class">
+                        </div>
+                        <div class="col-md-4">
+                            <div class="dropdown menu-form-button-type-dropdown">
+                                <a href="#" data-toggle="dropdown" class="btn btn-primary wn-icon-angle-down">Button type</a>
+
+                                <ul class="dropdown-menu" role="menu" data-dropdown-title="Add something small">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#" class="wn-icon-folder">Group</a></li>
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#" class="wn-icon-copy">Page</a></li>
+                                </ul>
+                            </div>
+                            <div class="menu-form-button-type-input">
+                                <input type="text" class="form-control" placeholder="Url" aria-label="Url">
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="checkbox custom-checkbox menu-form-target-blank">
+                                <input name="checkbox" value="1" type="checkbox" id="checkbox_1">
+                                <label for="checkbox_1">target="_blank"</label>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <select class="form-control custom-select">
+                                <option value="1" selected="selected">One</option>
+                                <option value="2">Two</option>
+                            </select>
+                        </div>
+
+                    </div>
+                    <label for="subMenu_123456">Sub menu</label>
+                    <div class="content-menu" id="subMenu_123456">
+
+                        <!-- Menu list -->
+                        <div class="menu-item">
+                            <div class="menu-header"><h3>#1 menu-header</h3><i class="icon-angle-right"></i></div>
+                            <div class="menu-body">
+                                FORM
+                            </div>
+                        </div>
+                        <div class="menu-item">
+                            <div class="menu-header"><h3>#2 menu-header</h3><i class="icon-angle-right"></i></div>
+                            <div class="menu-body">
+                                FORM
+                            </div>
+                        </div>
+
+                        <!-- Add buttons-->
+                        <div class="add-btns">
+                            <div class="btn-group" role="group" aria-label="Adding a menu">
+                                <button type="button" class="btn btn-outline-success wn-icon-add"><?= e(trans('forwintercms.content::widget.menu.add_item')) ?></button>
+                                <button type="button" class="btn btn-outline-warning wn-icon-ellipsis-h"><?= e(trans('forwintercms.content::widget.menu.add_separator')) ?></button>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
             <div class="menu-item">
                 <div class="menu-header"><h3>#2 menu-header</h3><i class="icon-angle-right"></i></div>
                 <div class="menu-body">
-                    FORM
+
+                    <div class="content-menu">
+
+                        <!-- Menu list -->
+                        <!-- EMPTY -->
+
+                        <!-- Add buttons-->
+                        <div class="add-btns">
+                            <div class="btn-group" role="group" aria-label="Adding a menu">
+                                <button type="button" class="btn btn-outline-success wn-icon-add"><?= e(trans('forwintercms.content::widget.menu.add_item')) ?></button>
+                                <button type="button" class="btn btn-outline-warning wn-icon-ellipsis-h"><?= e(trans('forwintercms.content::widget.menu.add_separator')) ?></button>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
             <!-- Add buttons-->
-            <div class="text-center add-btns">
+            <div class="add-btns">
                 <div class="btn-group" role="group" aria-label="Adding a menu">
                     <button type="button" class="btn btn-outline-success wn-icon-add"><?= e(trans('forwintercms.content::widget.menu.add_item')) ?></button>
                     <button type="button" class="btn btn-outline-warning wn-icon-ellipsis-h"><?= e(trans('forwintercms.content::widget.menu.add_separator')) ?></button>
