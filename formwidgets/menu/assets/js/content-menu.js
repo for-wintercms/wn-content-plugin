61d119575a5ae85d635b3aec01180425e7a89728
DiamondSystems <me@diamondsystems.org>
2024-02-23 14:41:11 +0500

    Animation of arrows when collapsing blocks.

--- formwidgets/menu/assets/js/content-menu.js
+++ formwidgets/menu/assets/js/content-menu.js
@@ -6,25 +6,50 @@ const contentMenu = {
 
     menuItemToggleAnimateInMs: 100,
 
-    events: function ()
+    toggleMenu: function()
     {
         const me = this;
 
-        // Toggle menu
         $('.field-contentmenu').on('click', '.content-menu > .menu-item > .menu-header', function()
         {
             if ($(this).hasClass('active'))
             {
-                $(this).removeClass('active').next().hide(me.menuItemToggleAnimateInMs);
+                $(this).removeClass('active')
+                    .next()
+                    .hide(me.menuItemToggleAnimateInMs);
+
+                $(this).children('i.icon-angle-down')
+                    .removeClass('icon-angle-down')
+                    .addClass('icon-angle-right');
+
                 return;
             }
 
             const $contentMenu = $(this).closest('.content-menu');
-            $contentMenu.find('.menu-header').removeClass('active');
-            $contentMenu.find('.menu-body').hide();
 
-            $(this).addClass('active').next().show(me.menuItemToggleAnimateInMs);
+            $contentMenu.find('.menu-header')
+                .removeClass('active');
+
+            $contentMenu.find('.menu-header > i.icon-angle-down')
+                .removeClass('icon-angle-down')
+                .addClass('icon-angle-right');
+
+            $contentMenu.find('.menu-body')
+                .hide();
+
+            $(this).addClass('active')
+                .next()
+                .show(me.menuItemToggleAnimateInMs);
+
+            $(this).children('i.icon-angle-right')
+                .removeClass('icon-angle-right')
+                .addClass('icon-angle-down');
         });
+    },
+
+    events: function ()
+    {
+        this.toggleMenu();
     },
 
     init: function ()
