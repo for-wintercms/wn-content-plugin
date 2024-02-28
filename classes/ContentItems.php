77d99c08018f32570866eee3aba240174220b241
Almaz <me@diamondsystems.org>
2024-02-28 14:05:05 +0500

    The getItemData and getItemsData methods have been added to get data in the backend

--- classes/ContentItems.php
+++ classes/ContentItems.php
@@ -4,6 +4,8 @@ namespace ForWinterCms\Content\Classes;
 
 use Lang;
 use Exception;
+use ForWinterCms\Content\Models\Page as PageModel;
+use ForWinterCms\Content\Classes\Traits\ContentItemsData;
 use ForWinterCms\Content\Classes\Interfaces\ContentItems as InterfaceContentItems;
 
 /**
@@ -15,6 +17,7 @@ class ContentItems implements InterfaceContentItems
 {
     use \Winter\Storm\Support\Traits\Singleton;
     use Traits\ContentItemsParse;
+    use ContentItemsData;
 
     protected function init()
     {
@@ -24,7 +27,13 @@ class ContentItems implements InterfaceContentItems
         catch (Exception $e){}
     }
 
-    public function checkPageSlug(string $pageSlug)
+    /**
+     * Check page slug
+     *
+     * @param string $pageSlug
+     * @return bool
+     */
+    public function checkPageSlug(string $pageSlug): bool
     {
         return ($pageSlug && isset($this->contentItemList[$pageSlug]));
     }
@@ -59,5 +68,54 @@ class ContentItems implements InterfaceContentItems
             return null;
         $partial = $this->contentItemsContentPath .'/'. preg_replace("/\.htm$/i", '', $partial) .'.htm';
         return file_exists($partial) ? $partial : null;
+    }
+
+    /**
+     * Get Item data
+     *
+     * @param string $pageSlug   Page slug
+     * @param string $itemSlug   Item slug (key)
+     * @param string|null $lang  Translation language
+     *
+     * @return string|array|null
+     */
+    public function getItemData(string $pageSlug, string $itemSlug, string $lang = null)
+    {
+        return $this->getItemsData($pageSlug, $itemSlug, $lang)[$itemSlug] ?? null;
+    }
+
+    /**
+     * Get Items data
+     *
+     * @param string      $pageSlug   Page slug
+     * @param array|string|null  $itemSlugs  Item slugs (keys)
+     * @param string|null $lang       Translation language
+     *
+     * @return array
+     * @throws
+     */
+    public function getItemsData(string $pageSlug, array|string $itemSlugs = null, string $lang = null): array
+    {
+        if (! $this->checkPageSlug($pageSlug))
+            return [];
+
+        $page = PageModel::slug($pageSlug)->first();
+        if (! $page)
+            return [];
+
+        $result = [];
+        $fnItems = function() use ($page, $itemSlugs, $lang) {
+            if (empty($itemSlugs))
+                return $page->items()->itemsLang($lang)->get();
+            else if (is_string($itemSlugs))
+                return $page->items()->where('name', $itemSlugs)->itemsLang($lang)->get();
+            else
+                return $page->items()->whereIn('name', $itemSlugs)->itemsLang($lang)->get();
+        };
+
+        foreach ($fnItems() as $item)
+            $result[$item->name] = $this->getContentItemsData($item);
+
+        return $result;
     }
 }
